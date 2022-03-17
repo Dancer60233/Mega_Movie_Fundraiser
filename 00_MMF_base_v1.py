c0cbe5d3622b2ca1b82d89b66295953164bbaf36
Kate Barber <101075108+Dancer60233@users.noreply.github.com>
2022-03-17 20:02:53 +0000

    I added my first version of the base sequence

---
+++ 00_MMF_base_v1.py
@@ -0,0 +1,29 @@
+#import statements
+
+
+#functions go here
+
+
+#************* Main Routine ****************
+
+#Set up dictionaries / lists needed to hold data
+
+#Ask user if they have used the program before and show instructions if nessecary
+
+#Loop to get ticket details
+
+ #Get name (Can't be blanl)
+
+ #Get age (Between 12 and 130)
+
+ #Calculate ticket price
+
+ #Loop to ask for snacks
+
+ #Calculate snack price
+
+ #ask for payment method (and apply surcharge if nesecracy)
+
+#Calculate total Sales and profit
+
+#Output data to text file
