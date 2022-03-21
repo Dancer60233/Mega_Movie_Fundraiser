6157083c92deb9242a27e688ae87f0af523e4009
Kate Barber <101075108+Dancer60233@users.noreply.github.com>
2022-03-21 02:04:56 +0000

    Changed Ticket Loop V2 code by getting the input after I said how many tickets were left. Made the code more accesible for users and more efficent and easier to understand

--- 02_ticket_loop_v2.py
+++ 02_ticket_loop_v2.py
@@ -9,16 +9,18 @@ count = 0
 MAX_TICKETS = 5
 
 while name != "xxx" and count < MAX_TICKETS:
+  print("You have {} seats left".format(MAX_TICKETS-count))
 
  #Get details....
- name = input("Name: ")
- if name != "xxx":
-  count += 1
-  if count < MAX_TICKETS:
-   print("You have {} seats left".format(MAX_TICKETS-count))
-  else:
-    print("You have sold all available tickets!")
- else:
-   print("You have sold {} tickets. There are {} places still available".format(count,MAX_TICKETS-count))
+  name = input("Name: ")
+  if name != "xxx":
+   count += 1
+
+
+if count == MAX_TICKETS:
+  print("You have sold all abailable tickets!")
+   
+else:
+   print("You have sold {} tickets. \n There are {} places still available".format(count,MAX_TICKETS-count))
    
   
