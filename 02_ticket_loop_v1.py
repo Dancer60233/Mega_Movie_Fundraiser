e93e69e97c43160c3af5972eef3d54c542c10951
Kate Barber <101075108+Dancer60233@users.noreply.github.com>
2022-03-21 01:36:14 +0000

    ticket loop V1 was created and tested

---
+++ 02_ticket_loop_v1.py
@@ -0,0 +1,15 @@
+#21/3/22
+#Kate barber
+# MMF Ticket loop- V1
+
+# start of loop
+
+name = ""
+count = 0
+MAX_TICKETS = 5
+
+while name != "xxx" and count < MAX_TICKETS:
+
+ #Get details....
+ name = input("Name: ")
+ count += 1
