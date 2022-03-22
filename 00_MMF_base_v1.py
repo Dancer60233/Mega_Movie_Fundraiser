80b2973202b1bae0091b8654ecba0f4a2d9e0c57
Kate Barber <101075108+Dancer60233@users.noreply.github.com>
2022-03-22 21:13:21 +0000

    Added the ticket loop to MMF Base

--- 00_MMF_base_v1.py
+++ 00_MMF_base_v1.py
@@ -27,9 +27,41 @@ def not_blank(question):
 #Ask user if they have used the program before and show instructions if nessecary
 
 #Loop to get ticket details
+#23/3/22
+#Kate barber
+# MMF Ticket loop- V3
 
- #Get name (Can't be blank)
- name= not_blank("Name: ")
+# start of loop
+
+name = ""
+count = 0
+MAX_TICKETS = 5
+
+while name != "xxx" and count < MAX_TICKETS:
+ #Tellsusers how many seats are left
+  if count < 4:
+    print("You have {} seats left".format(MAX_TICKETS-count))
+    
+  #Warns user that there is only one seat left
+  else:
+    print("*** There is ONE seat left!! ***")
+
+ #Get details....
+    
+  #Get name (Can't be blank)
+  name= not_blank("Name: ")
+  if name != "xxx":
+   count += 1
+
+
+if count == MAX_TICKETS:
+  print("You have sold all available tickets!")
+   
+else:
+   print("You have sold {} tickets. \nThere are {} places still available".format(count,MAX_TICKETS-count))
+   
+  
+
 
  #Get age (Between 12 and 130)
 
