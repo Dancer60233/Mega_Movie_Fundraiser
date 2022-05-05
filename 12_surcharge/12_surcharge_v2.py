c405b08739919a5a204b7388f03280c10a023672
Kate Barber <101075108+Dancer60233@users.noreply.github.com>
2022-05-05 21:34:17 +0000

    Created 12_surcharge_v2 and got the error message to work

---
+++ 12_surcharge/12_surcharge_v2.py
@@ -0,0 +1,67 @@
+
+
+#Function goes here
+#WARNING: The response will be returned in title case
+
+def string_check(choice, options):
+
+ for var_list in options:
+   #if snack is in one of the lists, return the full name
+      if choice in var_list:
+  #Get full name of snack and put it in titles case so it looks nice when outputted
+       return var_list[0].title()
+       is_valid = "yes"
+       break
+
+      else:
+        is_valid = "no"
+
+ if is_valid == "yes":
+    return chosen
+
+ else:
+    return "invalid choice"
+
+
+ 
+
+
+
+
+
+pay_method = [
+  ["cash", "ca"],
+  ["credit", "cr"],
+]
+
+name = ""
+SURCHARGE = 2
+
+#loop till exit code
+while name != "xxx":
+  name = input("Name: ")
+  if name == "xxx":
+    break
+
+  #ask for payment method
+  how_pay = "invalid choice"
+  while how_pay == "invalid choice":
+   how_pay = input("Please choose a payment method (cash or credit) ").lower()
+   how_pay = string_check(how_pay, pay_method)
+   if how_pay == "invalid choice":
+     print("Please enter a valid choice")
+ 
+  #ask for a subtotal (for testing purposes)
+  subtotal = float(input("Sub total? $"))
+  if how_pay == "Credit":
+    surcharge = 0.05 * subtotal
+  else: 
+    surcharge = 0
+
+
+  total = subtotal + surcharge 
+
+  print("Name: {} | Subtotal: ${:.2f} | Surcharge: ${:.2f} | \nTotal Payable: ${:.2f} \n".format(name, subtotal, surcharge, total))
+
+
+
